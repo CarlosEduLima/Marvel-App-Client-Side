e18a2f15c861fc3dc38946bcaa53eb3247fb4d78
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-02-26 07:26:31 -0300

    create and test protected routes and authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import GlobalStyles from './styles/GlobalStyles';
 import React from 'react';
 import Login from './pages/Login'
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect, Link } from "react-router-dom";
+import {isAuthenticated, logout} from './services/auth'
+
+export const ProtectedRoute = ({ component: Component, path,...rest}) => {
+  return (
+      <Route
+          {...rest}
+          render={props => {
+              if (isAuthenticated()) {
+                  return <Component {...props} />;
+              } else {
+                  return (
+                      <Redirect to={{ pathname: "/login", state: {
+                          prevLocation: path,
+                          error: "You need to login first!",
+                        },}}/>
+                  );
+              }
+          }}
+      />
+  );
+};
+
+function PrivateComponent(){
+  return(
+    <>
+    <h1>I'm Private</h1>
+    <Link to="/login">
+    <button onClick={() => logout()} style={{width:'40px', height:'40px', background:'blue'}}>Logout</button>
+    </Link>
+    </>
+  )
+}
 
 function App() {
   return (
@@ -9,6 +41,7 @@ function App() {
      <BrowserRouter>
             <Switch>
                 <Route path="/login" component={Login} />
+                <ProtectedRoute path="/protect-route" component={PrivateComponent} />
             </Switch>
         </BrowserRouter>
     <GlobalStyles />
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,32 @@
-import {Container, Logo, Form, Input, Button, Footer, LinkButton} from './style.js'
-import Marvel from '../../assets/marvel_logo.png'
-import { Link } from 'react-router-dom'
+import {
+  Container,
+  Logo,
+  Form,
+  Input,
+  Button,
+  Footer,
+  LinkButton,
+} from "./style.js";
+import Marvel from "../../assets/marvel_logo.png";
+import { Link } from "react-router-dom";
+import { login } from "../../services/auth";
 
-function Login() {
+function Login({ history }) {
+  function handleSubmit() {
+    console.log('i was called')
+    login("ldlskfçlpewekrwekrwlçerkwçelkr");
+    history.push("/protect-route");
+  }
   return (
     <Container>
-        <Logo src={Marvel}/>
-        <Form>
-          <Input  placeholder="Email" />
-          <Input placeholder="Senha" />
-          <Button>Login</Button>
-              <span>Não possui cadastro?</span>
-              <LinkButton>clique aqui</LinkButton>
-        </Form>
+      <Logo src={Marvel} />
+      <Form>
+        <Input placeholder="Email" />
+        <Input placeholder="Senha" />
+        <Button onClick={() => handleSubmit()}>Login</Button>
+        <span>Não possui cadastro?</span>
+        <LinkButton>clique aqui</LinkButton>
+      </Form>
     </Container>
   );
 }
