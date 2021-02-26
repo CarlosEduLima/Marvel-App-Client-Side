4a89f39193d922368ab6424325eb391c7d4a6f17
Carlos Eduardo <carlos.lima.exe@gmail.com>
2021-02-25 21:49:56 -0300

    include browserRouter in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import GlobalStyles from './styles/GlobalStyles';
 import React from 'react';
 import Login from './pages/Login'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
     <>
-    <Login />
+     <BrowserRouter>
+            <Switch>
+                <Route  path="/login" component={Login} />
+            </Switch>
+        </BrowserRouter>
     <GlobalStyles />
   </>
   );
